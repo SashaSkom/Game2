489e2f7862d993b9cae7b1a5fc3ccd6be710020a
SergeyBirukov <72649075+SergeyBirukov@users.noreply.github.com>
2024-03-27 14:59:44 +0500

    Main Menu (#3)

---
+++ Assets/Scripts/UI/MainMenu.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace UI
+{
+    public class MainMenu: MonoBehaviour
+    {
+        public string startScene = "SampleScene";
+    
+        public void StartGame()
+        {
+            SceneManager.LoadScene(startScene);
+        }
+
+        public void QuitGame()
+        {
+            Debug.Log("Quit");
+            Application.Quit();
+        }
+    }
+}
