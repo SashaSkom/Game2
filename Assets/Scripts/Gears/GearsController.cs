52f0b3caa87865c38a719c95dfee97fc00cfb8e8
Сергей Бирюков <birukovsergej121@gmail.com>
2024-05-14 18:35:57 +0500

    Add collectable gears

---
+++ Assets/Scripts/Gears/GearsController.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using UnityEngine.Events;
+
+namespace Gears
+{
+    public class GearsController: MonoBehaviour
+    {
+        [SerializeField] public UnityEvent onGearCollected = new();
+        [SerializeField] public UnityEvent<int> onTrySpendGears = new();
+        [SerializeField] public UnityEvent notEnoughGears = new();
+
+        public int GearsCount { get; private set; }
+
+        private void Start()
+        {
+            onGearCollected.AddListener(OnGearCollectedHandler);
+            onTrySpendGears.AddListener(OnTrySpendGearsHandler);
+        }
+
+        private void OnGearCollectedHandler()
+        {
+            GearsCount++;
+        }
+        
+        private void OnTrySpendGearsHandler(int count)
+        {
+            if (count <= GearsCount)
+            {
+                GearsCount -= count;
+            }
+            else
+            {
+                notEnoughGears.Invoke();
+            }
+        }
+    }
+}
