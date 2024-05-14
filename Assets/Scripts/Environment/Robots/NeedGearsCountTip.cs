cf8c618bed24ecc80187b62c789d941006e427de
Сергей Бирюков <birukovsergej121@gmail.com>
2024-05-14 21:57:08 +0500

    Add gears count above robot

--- Assets/Scripts/Environment/Robots/NeedGearsCountTip.cs
+++ Assets/Scripts/Environment/Robots/NeedGearsCountTip.cs
@@ -1,9 +1,24 @@
-﻿using UnityEngine;
+﻿using TMPro;
+using UnityEngine;
 
 namespace Environment.Robots
 {
     public class NeedGearsCountTip: MonoBehaviour
     {
-        
+        [SerializeField] private TextMeshProUGUI countText;
+        [SerializeField] private Robot robot;
+
+        private void Start()
+        {
+            countText.text = $"{robot.NeedGearsToRepair}";
+        }
+
+        private void Update()
+        {
+            if (!robot.isBroken)
+            {
+                gameObject.SetActive(false);
+            }
+        }
     }
 }
