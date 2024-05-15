0876704230bb9f9b975a8266e6a54e28fc87d1b5
SergeyBirukov <72649075+SergeyBirukov@users.noreply.github.com>
2024-05-15 15:39:26 +0500

    Add gears spawn (#10)

---
+++ Assets/Scripts/Gears/OnDestroyGearsSpawner.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+namespace Gears
+{
+    public class OnDestroyGearsSpawner: MonoBehaviour
+    {
+        [SerializeField] private GameObject gearPrefab;
+        [SerializeField] private List<Transform> spawnPoints;
+
+        private void OnDestroy()
+        {
+            Debug.Log("OnDestroy");
+            foreach (var spawnPoint in spawnPoints)
+            {
+                Debug.Log("inst");
+                Instantiate(gearPrefab, spawnPoint);
+            }
+        }
+    }
+}
