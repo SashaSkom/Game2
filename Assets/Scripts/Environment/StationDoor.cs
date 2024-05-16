77c313243be0e109c976bc177e83ec73771f8cba
SergeyBirukov <72649075+SergeyBirukov@users.noreply.github.com>
2024-05-16 18:17:29 +0500

    Mini games at level 1 (#12)
    * Add first mini game
    
    * Add gears drop
    
    * Add light game to level1
    
    * Add enter inside station

--- Assets/Scripts/Environment/StationDoor.cs
+++ Assets/Scripts/Environment/StationDoor.cs
@@ -8,11 +8,16 @@ namespace Environment
         [SerializeField] private GameObject doorMiniGame;
         [SerializeField] private CharacterEvents characterEvents;
         [SerializeField] private bool canInteract = true;
+        [SerializeField] private Transform insideStationPosition;
+        [SerializeField] private Character character;
+        [SerializeField] private CameraController cameraController;
 
         private void Start()
         {
             var gameEvents = FindObjectOfType<DoorGameEvents>().GetComponent<DoorGameEvents>();
             characterEvents = FindObjectOfType<CharacterEvents>().GetComponent<CharacterEvents>();
+            character = FindObjectOfType<Character>().GetComponent<Character>();
+            cameraController = FindObjectOfType<CameraController>().GetComponent<CameraController>();
             gameEvents.onWinGame.AddListener(OnWinGameHandler);
         }
 
@@ -25,13 +30,14 @@ namespace Environment
         public void EndMiniGame()
         {
             doorMiniGame.SetActive(false);
-            characterEvents.enableMovements.Invoke();
-            canInteract = true;
+            canInteract = false;
         }
 
         public void OnWinGameHandler()
         {
             EndMiniGame();
+            characterEvents.enableMovements.Invoke();
+            MovePlayerInsideStation();
             gameObject.SetActive(false);
         }
 
@@ -45,6 +51,13 @@ namespace Environment
             if(doorMiniGame.activeSelf || !canInteract) return;
             canInteract = false;
             StartMiniGame();
+        }
+
+        private void MovePlayerInsideStation()
+        {
+            character.transform.position = insideStationPosition.position;
+            cameraController.transform.position = new Vector3(insideStationPosition.position.x,
+                insideStationPosition.position.y, cameraController.transform.position.z);
         }
     }
 }
