ae88ff7d04751536923f7f19a8b6ae7e9ee5594c
SergeyBirukov <72649075+SergeyBirukov@users.noreply.github.com>
2024-05-16 21:28:34 +0500

    Add control tips + some fixes (#14)

---
+++ Assets/Scripts/UI/ControlsTip/ControlsTipTrigger.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+
+namespace UI.ControlsTip
+{
+    public class ControlsTipTrigger: MonoBehaviour
+    {
+        [SerializeField] private string tipText;
+        [SerializeField] private ControlsTip controlsTip;
+    
+        private void Start()
+        {
+            controlsTip = FindObjectOfType<ControlsTip>().GetComponent<ControlsTip>();
+        }
+
+        private void OnTriggerEnter2D(Collider2D col)
+        {
+            controlsTip.ShowTipText(tipText);
+        }
+
+        private void OnTriggerExit2D(Collider2D other)
+        {
+            controlsTip.Hide();
+        }
+    }
+}
