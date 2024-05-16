e972b50f0877c3b69dfbb6b138d3c1da6b7caddb
SergeyBirukov <72649075+SergeyBirukov@users.noreply.github.com>
2024-05-16 13:17:13 +0500

    Add falling tree (#11)

---
+++ Assets/Scripts/Environment/FallingTree/FallingTree.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+
+namespace Environment.FallingTree
+{
+    public class FallingTree: MonoBehaviour
+    {
+        [SerializeField] private Collider2D treeCollider;
+        [SerializeField] private Rigidbody2D rigidbody2D;
+        private const int DefaultLayer = 0;
+        
+        private void Start()
+        {
+            rigidbody2D = GetComponent<Rigidbody2D>();
+        }
+        
+        private void OnTriggerEnter2D(Collider2D col)
+        {
+            if (col.CompareTag("FallingTargetPoint"))
+            {
+                Debug.Log("Trigger enter");
+                treeCollider.gameObject.layer = DefaultLayer;
+                treeCollider.gameObject.tag = "Ground";
+                rigidbody2D.constraints = RigidbodyConstraints2D.FreezePosition;
+            }
+        }
+    }
+}
