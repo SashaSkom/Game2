5b0e9bc5d9f6bd9b34bce5069f1450e77a02789d
Сергей Бирюков <birukovsergej121@gmail.com>
2024-05-16 21:27:47 +0500

    Add control tips + some fixes

---
+++ Assets/Scripts/UI/ControlsTip/ControlsTip.cs
@@ -0,0 +1,21 @@
+﻿using TMPro;
+using UnityEngine;
+
+namespace UI.ControlsTip
+{
+    public class ControlsTip: MonoBehaviour
+    {
+        [SerializeField] private TextMeshProUGUI tipText;
+
+        public void ShowTipText(string text)
+        {
+            tipText.gameObject.SetActive(true);
+            tipText.text = text;
+        }
+
+        public void Hide()
+        {
+            tipText.gameObject.SetActive(false);
+        }
+    }
+}
